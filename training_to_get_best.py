db8a014d84177faec66702df6c34be711fbd1068
Rohit <rohitkale523@gmail.com>
2024-01-30 12:57:42 +0530

    Training to get best model and prediction

---
+++ training_to_get_best.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+
+
+source_path = '/home/rohit/GithubRepo/Object-detection--Roboflow/footballPlayers/test/images'
+# Create a new YOLO model from scratch
+model = YOLO('yolov8n.yaml')
+
+# Train the model using the 'coco128.yaml' dataset for 3 epochs
+results = model.train(data='footballPlayers/data.yaml', epochs=20,)
+
+# Evaluate the model's performance on the validation set
+results = model.val()
+
+model.predict(source= source_path, save = True)
